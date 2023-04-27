d1340956c7b00cd3ad54d575baf6facec3f1979d
Melody Davis <piano.fan122@gmail.com>
2023-04-27 10:49:21 -0400

    Initial Commit

---
+++ FaceIt/FaceItApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceItApp.swift
+//  FaceIt
+//
+//  Created by Melody Davis on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct FaceItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
